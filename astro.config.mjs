e04f36d29c1945f6205eafbab34bac4a12f60be3
Colin Sproule <colinsproule@gmail.com>
2026-03-28 00:31:46 -0700

    remove sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import NetlifyCMS from "astro-netlify-cms";
 import dcapConfig from "./decap.config.mjs";
@@ -24,7 +23,6 @@ export default defineConfig({
       ],
     }),
     mdx(),
-    sitemap(),
     tailwind(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
